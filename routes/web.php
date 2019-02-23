d392393de811995ea6265269081d78590fa5ad85
Mohamed <mohamed505118@aswu.edu.com>
2019-02-24 00:00:39 +0200

    crud for moderator, companies & clients

--- routes/web.php
+++ routes/web.php
@@ -13,4 +13,25 @@
 
 Route::get('/', function () {
     return view('welcome');
+
 });
+
+
+Route :: get('moderator/store/{uid}/{cid}','moderators@store');
+Route :: get('moderator/show/{id}','moderators@show');
+Route :: get('moderator/update/{uid}/{cid}','moderators@update');
+Route :: get('moderator/destroy/{id}','moderators@destroy');
+Route :: get('client/store/{id}/{name}/{email}/{company_id}','clients@store');
+Route :: get('client/show/{id}','clients@show');
+Route :: get('client/update/{id}/{name}/{email}/{company_id}','clients@update');
+Route :: get('client/destroy/{id}','clients@destroy');
+Route :: get('company/store/{id}/{name}','companies@store');
+Route :: get('company/show/{id}','companies@show');
+Route :: get('company/update/{id}/{name}','companies@update');
+Route :: get('company/destroy/{id}','companies@destroy');
+
+//Route::get('client/{id}', [
+  //  'as' => 'client.show',
+  //  'uses' => 'clients@show'
+//]);
+//Route::resource('user', 'users', ['except' => 'show']);
