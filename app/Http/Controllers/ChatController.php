9e97b51e3f6d7226f21403f835557be3ef5ae56e
Hossam Mohammed <hossammohammed72@gmail.com>
2019-04-19 14:47:01 +0200

    chat feature

--- app/Http/Controllers/ChatController.php
+++ app/Http/Controllers/ChatController.php
@@ -6,24 +6,31 @@ use Illuminate\Http\Request;
 use Chatkit\Chatkit;
 use App\Models\Agent;
 use App\Models\Client;
+use App\Models\Company;
 class ChatController extends Controller
 {
     //
     public function __construct(){
         $this->chatkit = new ChatKit(['instance_locator'=>env('PUSHER_INSTANCE_LOCATOR'),'key'=>env('PUSHER_KEY')]);
     }
-    public function ِaddClientToRoom($user){
+    public function ِaddClientToRoom(Request $request){
 
         $agent= Agent::with('user')->first();
-        $this->chatkit->createUser([
-            'id'=>$user,
-            'name'=>'Hossam'
-        ]);
+       if(Client::where('email',$request->email)->count() == 0)
+        {
+            $client = new Client();
+            $client->email = $request->email;
+            $client->name = $request->name;
+            $client->company_id = Company::InRandomOrder()->first()->id;
+            $client->save();
+            $this->chatkit->createUser(['id'=>$request->email,'name'=>$request->name]);
+        }
         $this->chatkit->createRoom([
             'creator_id'=>$agent->user->email,
             'name'=>'Servatic',
-            'user_ids'=>[$user],
+            'user_ids'=>[$request->email],
         ]);
+        return response()->json(['msg'=>'success'],200);
 
     }
     public function testAgentUser(){
